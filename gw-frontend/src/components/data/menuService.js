8ec4fad9fe599b36259f521a01f12201a65f71ca
4IPE <bykov220033@gmail.com>
2024-11-17 00:26:01 +0300

    <fix>изменение загрузки данных на фронтенд

--- gw-frontend/src/components/data/menuService.js
+++ gw-frontend/src/components/data/menuService.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 // Функция для получения данных меню
 export const fetchMenuItems = async () => {
+    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
     try {
-        const response = await axios.get('/gw-service/products/all');
+        const response = await axios.get(`${API_BASE_URL}/products/all`);
         return response.data; // Возвращаем данные меню
     } catch (error) {
         console.error("Ошибка при загрузке данных меню:", error);
